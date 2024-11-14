a01ef8d52772a439c45e0211900b7242bc845287
sebasmayor06 <sebda06@gmail.com>
2024-11-13 22:11:51 -0500

    subo cambios pedidos por el pastor

---
+++ src/pages/Landingpage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Landingpage() {
+  return (
+    <div className='flex flex-col justify-center items-center bg-slate-900 text-white w-screen min-h-screen gap-6'>
+        <img src="../img/logo.jpg" alt="" className='w-60 h-60 rounded-full'/>
+        <h1>BIENVENIDOS A LA IGLESIA CCI</h1>
+    </div>
+  )
+}
